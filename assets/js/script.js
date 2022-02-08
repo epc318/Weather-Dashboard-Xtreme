f382b9dfbfdfa6287fbf9e87e9acb12f4e09ba35
Rick Johnson <carl4917@umn.edu>
2022-02-08 01:06:29 -0600

    adding openweather API and activating button

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,23 @@ let currentForecast = function(location, data) {
     wind.textContent = data.wind.speed + 'mph';
     humidity.textContent = data.main.humidity + '%';
 };
+
+let submitAPI = function(event) {
+    event.preventDefault();
+    let apiUrl ='http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=bc541e0acdc6372acb78d3865f34ac3b';
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+            currentForecast(location, data);
+            })
+        }
+        else {
+            console.log('The city you searched does not exist, please try again.');
+        }
+    }); 
+};
+document.querySelector('#searchbtn').addEventListener('click', function() {
+    let input = document.querySelector('#city').value;
+    input.textContent = "PLACEHOLDER";
+    formSubmit();
+});
